6991b88ebe74fdc5548bd5622c342b4ff7d346e4
wangshouyan1 <2689673207@qq.com>
2020-06-07 17:42:02 +0800

    update

--- 系统(2) 55 班元鹏/Camera 5/EditorCamera.cs
+++ 系统(2) 55 班元鹏/Camera 5/EditorCamera.cs
@@ -24,11 +24,11 @@ namespace Gamekit3D.Cameras
 
         void Update()
         {
-            mouseDelta = Input.mousePosition - mousePosition;
-            mousePosition = Input.mousePosition;
+            mouseDelta = Input.mousePosition - mousePosition;//对增量进行赋值
+            mousePosition = Input.mousePosition;//重新定义鼠标的位置
 
-            if (Input.GetKey(KeyCode.W))
-                velocity.z += Time.deltaTime * (Input.GetKey(KeyCode.LeftShift) ? 10 : 3);
+            if (Input.GetKey(KeyCode.W))//获取W键
+                velocity.z += Time.deltaTime * (Input.GetKey(KeyCode.LeftShift) ? 10 : 3);//移动的速度。。leftshift是什么？？？？？
             else if (Input.GetKey(KeyCode.S))
                 velocity.z -= Time.deltaTime * (Input.GetKey(KeyCode.LeftShift) ? 10 : 3);
             else
@@ -41,17 +41,18 @@ namespace Gamekit3D.Cameras
                 velocity.x *= Time.deltaTime;
 
 
-            if (Input.GetMouseButton(1))
-            {
-                angles.x += mouseDelta.y;
+            if (Input.GetMouseButton(1))//获取鼠标按键
+            {//重点：mouseDelta是鼠标的增量，鼠标移动是是平面移动，只有x和y，鼠标向Y轴移动时，游戏中视角绕X轴旋转，同理鼠标向X轴移动，游戏中的视角绕y轴移动
+                angles.x += mouseDelta.y;//角度等于鼠标增量//
                 angles.y += mouseDelta.x;
             }
 
-            transform.Translate(velocity * Time.deltaTime, Space.Self);
+            transform.Translate(velocity * Time.deltaTime, Space.Self);//向某方向移动某物体
 
-            var yaw = Quaternion.AngleAxis(angles.y, Vector3.up);
+            var yaw = Quaternion.AngleAxis(angles.y, Vector3.up);//Var类型预先不用知道变量的类型；根据你给变量赋值来判定变量属于什么类型
             var pitch = Quaternion.AngleAxis(angles.x, Vector3.left);
-            transform.localRotation = originRotation * yaw * pitch;
+            transform.localRotation = originRotation * yaw * pitch;//Transform.localRotation 局部旋转//该变换的旋转角度相对于父级变换的旋转角度。 
+
 
         }
     }
